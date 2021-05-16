c301c47110a9ed58d066227fe63fb76b1c5a441f
salih <salih@192.168.1.20>
2021-05-16 16:48:22 +0300

    16.05

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "jobPositions")
+public class JobPosition {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	public JobPosition() {
+
+	}
+
+	public JobPosition(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+}
